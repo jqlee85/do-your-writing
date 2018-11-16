0a69d96830c4c434117783eb45c26d99ba4ce92b
Jesse Lee <jqlee85@gmail.com>
2018-11-16 13:53:49 -0300

    add initial code

---
+++ content.js
@@ -0,0 +1,13 @@
+console.log('You are on Facebook!');
+
+var extensionOrigin = 'chrome-extension://' + chrome.runtime.id;
+if (!location.ancestorOrigins.contains(extensionOrigin)) {
+    var iframe = document.createElement('iframe');
+    // Must be declared at web_accessible_resources in manifest.json
+    iframe.src = chrome.runtime.getURL('frame.html');
+
+    // Some styles for a fancy sidebar
+    iframe.style.cssText = 'position:fixed;top:0;left:0;display:block;' +
+                           'width:100%;height:100%;z-index:1000;';
+    document.body.appendChild(iframe);
+}
